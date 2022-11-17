6b0a56bec53798c07b42957d6dc6b0bb60a6d1db
Dmytro Hushchyk <107995555+DHushchyk@users.noreply.github.com>
2022-11-17 19:54:06 +0200

    Secret key changed

--- items_service/settings.py
+++ items_service/settings.py
@@ -22,7 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY",
+    "django-insecure-!(q9wzu_xd*n5^^g#ia7-62g+7lq%rx!nw&%zz55s67ade36(e",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
